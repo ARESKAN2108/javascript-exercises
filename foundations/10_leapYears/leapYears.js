8c5ced69c1f8f983646f3a1abdafe84dbbfcf54e
Maksim <85465078+ARESKAN2108@users.noreply.github.com>
2026-01-09 20:30:46 +0300

    leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const yearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
 
+    if(yearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
